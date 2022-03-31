01212a1d9bd9dec66b11e3da2d0abeb22d329fe8
realshurupovert <denisoff.ro@yandex.ru>
2022-03-31 16:03:40 +0000

    fixed script behaviour!

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -6,3 +6,9 @@ else
     echo -e "$dir directory does not exist, exiting."
     exit 101
 fi
+echo -e "The heaviest subfolder of $dir is:"
+top1=$(du -S $dir 2>/dev/null| sort -rn | head -n 1 | cut -f2)
+disk=$(du -Sh -d 0 $top1 2>/dev/null| sort -rn | head -n 1 | cut -f1)
+echo -e "$top1 with disk usage of $disk"
+echo "The heaviest subfolder of $dir is $top1" | festival --tts
+
